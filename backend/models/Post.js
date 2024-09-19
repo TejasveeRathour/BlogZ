95ea9ff288471accbfccb333a967dc303cf9d334
Tejasvee <tejasvee.rathour29aug@gmail.com>
2024-09-19 19:54:56 +0530

    Adding Model of Post

---
+++ backend/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const {Schema,model} = mongoose;
+
+const PostSchema = new Schema({
+  title:String,
+  summary:String,
+  content:String,
+  cover:String,
+  authorId:{
+    type:Schema.Types.ObjectId,
+    ref:'User'
+  },
+  likeduser: {
+    type: [String], 
+    default: []
+  },
+  savedPost: {
+    type: [String], 
+    default: []
+  },
+  likes: {
+    type: Number, 
+    default: 0
+  },
+}, {
+  timestamps: true,
+});
+
+const PostModel = model('Post', PostSchema);
+
+module.exports = PostModel;
