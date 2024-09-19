3111f44149809eb743b6381fa1debc7ccdd893fa
Tejasvee <tejasvee.rathour29aug@gmail.com>
2024-09-19 22:02:23 +0530

    editing in Validate update profile middleware

--- backend/middleware/ValidateUpdateProfilePhoto.js
+++ backend/middleware/ValidateUpdateProfilePhoto.js
@@ -1,25 +1,18 @@
+const validateUpdateProfilePhoto = (req, res, next) => {
+  // console.log("check1");
+  const expectedFileType = ["png", "jpg", "jpeg", "webp"];
+  if (!req.file) {
+    next();
+  } else {
+    const { originalname } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
 
-const validateUpdateProfilePhoto = (req,res,next)=>{
-    
-    // console.log("check1");
-    const expectedFileType = ['png', 'jpg', 'jpeg', 'webp'];
-    if(!req.file)
-    {
-        next();
+    if (!expectedFileType.includes(ext)) {
+      return res.status(403).json({ msg: "You can only upload Image" });
     }
-    else
-    {
-        const {originalname} = req.file;
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-    
-        if(!expectedFileType.includes(ext))
-        {
-            return res.status(403).json({"msg": "You can only upload Image"});
-        }
-        
-        next();
-    }
-   
-}
-module.exports = {validateUpdateProfilePhoto};
+
+    next();
+  }
+};
+module.exports = { validateUpdateProfilePhoto };
