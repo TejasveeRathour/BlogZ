b81d3cda9db0a0784b14d3281e1638c48a726e14
Tejasvee <tejasvee.rathour29aug@gmail.com>
2024-09-19 19:57:49 +0530

    Adding Model of Notification

---
+++ backend/models/Notification.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const {Schema,model} = mongoose;
+
+const  NotificationSchema = new Schema(
+{
+    notification_type: { 
+        type: String, 
+        enum: ['like', 'comment'], 
+        required: true 
+    },
+    message:  { 
+        type: String
+    },
+    userId:  { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User' 
+    },
+    postId:  { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post' 
+    },
+    authorId: { 
+        type:Schema.Types.ObjectId, 
+        ref:'User'
+    },
+},{
+    timestamps: true,
+});
+
+const NotificationModel = model('Notification',NotificationSchema);
+module.exports = NotificationModel;
