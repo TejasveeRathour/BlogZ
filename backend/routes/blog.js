85182f4aee292a98bbb4507cc281186dc873b8c1
Tejasvee <tejasvee.rathour29aug@gmail.com>
2024-09-19 21:00:21 +0530

    Adding routes, middleware and controllers related to edit post

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const multer = require('multer');
 const uploadMiddleware = multer({dest: 'uploads/'}); 
 const { validateCreatePost }  = require('../middleware/createPostValidator');
+const {validateEditPost} = require('../middleware/editPostValidator');
 const verifyToken = require('../middleware/verifyToken');
 
 const {doCreatePost} = require('../controllers/blog-controllers');
 
 // add post
 router.route('/post').post(verifyToken ,uploadMiddleware.single('file'),validateCreatePost, doCreatePost);
+
+// update a post 
+router.route('/updatepost').put(verifyToken, uploadMiddleware.single('file'),validateEditPost, doUpdatePost);
+
+module.exports = router;
